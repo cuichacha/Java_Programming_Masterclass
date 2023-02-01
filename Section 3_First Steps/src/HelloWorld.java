9d495f2c49a0503eabc3ebe1c033e9bdd1d68a4b
Will Dufresne <willdufresne@Wills-New-MacBook-Pro.local>
2023-02-01 19:49:09 +0800

    hello world

---
+++ Section 3_First Steps/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Will Dufresne
+ * @Date: 2023-01-02 10:23
+ * @Description: First class in this learning
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
