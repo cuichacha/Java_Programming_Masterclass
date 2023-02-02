c19ab4565a16ff8c55555e5c2f4da1dbb12bb8fb
Will Dufresne <chxdtc123@gmail.com>
2023-02-03 03:13:02 +0800

    Add the unique identifier

--- Section 10_List ArrayList LinkedList Iterator Autoboxing/src/TheAutoboxingUnboxingChallenge/Customer.java
+++ Section 10_List ArrayList LinkedList Iterator Autoboxing/src/TheAutoboxingUnboxingChallenge/Customer.java
@@ -1,11 +1,21 @@
 package TheAutoboxingUnboxingChallenge;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Will Dufresne
  * @Date: 2023-02-02 19:48
  * @Description: The customer class in the autoboxing & unboxing challenge
  */
-public record Customer(String name, List<Double> transactions) {
+public record Customer(UUID Id, String name, List<Double> transactions) {
+    public Customer(UUID Id, String name, List<Double> transactions) {
+        this.Id = UUID.randomUUID();
+        this.name = name;
+        this.transactions = transactions;
+    }
+
+    public Customer(String name, List<Double> transactions) {
+        this(null, name, transactions);
+    }
 }
--- Section 10_List ArrayList LinkedList Iterator Autoboxing/src/TheAutoboxingUnboxingChallenge/TheAutoboxingUnboxingChallenge.java
+++ Section 10_List ArrayList LinkedList Iterator Autoboxing/src/TheAutoboxingUnboxingChallenge/TheAutoboxingUnboxingChallenge.java
@@ -12,11 +12,14 @@ public class TheAutoboxingUnboxingChallenge {
     public static void main(String[] args) {
         Customer customer1 = new Customer("1", new ArrayList<>(List.of(-1.0, 2.0)));
         Customer customer2 = new Customer("2", new ArrayList<>(List.of(-2.0, 3.0)));
+        Customer customer3 = new Customer("3", new ArrayList<>(List.of(-2.0, 3.0)));
 
         Bank bank = new Bank("Bank", new ArrayList<>());
         bank.addCustomer(customer1);
+        bank.addCustomer(customer2);
         bank.addTransactions(customer1, 5.0);
         bank.printTransactions(customer1);
         bank.printTransactions(customer2);
+        bank.printTransactions(customer3);
     }
 }
